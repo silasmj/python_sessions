c29e1aea40f1e3cf77acaca7a2a3e8539a43a319
silasmj <silasmj@icloud.com>
2022-04-26 01:30:55 +0200

    generators practice

---
+++ generators.py
@@ -0,0 +1,31 @@
+def mygenerator():
+    print('First item')
+    yield 10
+
+    #   return
+
+    print('Second item')
+    yield 20
+
+    print('Last item')
+    yield 30
+
+
+gen = mygenerator()
+
+next(gen)
+next(gen)
+next(gen)
+
+
+def get_sequence_upto(x):
+    for i in range(x):
+        yield i
+
+
+gen=get_sequence_upto(5)
+while True:
+    try:
+        print ("Received on next(): ", next(gen))
+    except StopIteration:
+        break
